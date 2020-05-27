6da54a7997b8d07092019891b3448aef1d5df63c
mattyturn95 <mattyturn95@gmail.com>
2020-05-27 21:51:47 +0000

    added a model to products

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
