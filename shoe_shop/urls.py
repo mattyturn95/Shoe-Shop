ff312d7cfffcd8c6fa66fc89f790f20546f2cc6e
mattyturn95 <mattyturn95@gmail.com>
2020-05-29 12:58:06 +0000

    removed posts from project

--- shoe_shop/urls.py
+++ shoe_shop/urls.py
@@ -23,9 +23,7 @@ from checkout import urls as urls_checkout
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
-from django.views.generic import RedirectView
-from posts import urls as urls_posts
-from posts.views import get_posts, post_detail, create_or_edit_post
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -34,12 +32,6 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
-    url(r'^search/', include(urls_search)),
-    url(r'^create_or_edit_post/', create_or_edit_post, name="create_or_edit_post"),
-    url(r'^post_detail/', post_detail, name="post_detail"),
-    url(r'^get_posts/', get_posts, name="get_posts"),
-    url(r'^post$', RedirectView.as_view(url='posts/')),
-    url(r'^posts/', include('posts.urls')),
-   
+    url(r'^search/', include(urls_search)), 
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
